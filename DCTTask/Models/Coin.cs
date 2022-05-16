03645ce723792cc7bc4667243a8f223b97d6f298
Pavlo Pryima <pashaprujma@gmail.com>
2022-05-16 10:48:54 +0300

    added models

---
+++ DCTTask/Models/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCTTask.Models
+{
+    internal class Coin
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public double current_price { get; set; }
+        public double market_cap_rank { get; set; }
+        public double total_volume { get; set; }
+        public double price_change_24h { get; set; }
+        
+    }
+}
---
+++ DCTTask/Models/Market.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCTTask.Models
+{
+    internal class Market
+    {
+        public string coin_id { get; set; }        
+        public string name { get; set; }
+        public double converted_last { get; set; }
+    }
+}
