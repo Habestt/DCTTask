f2993cc68ab0fcc14e7daea4a74a69ca039f311d
Pavlo Pryima <pashaprujma@gmail.com>
2022-05-16 11:13:13 +0300

    added services

---
+++ DCTTask/Services/CoinService.cs
@@ -0,0 +1,41 @@
+﻿using DCTTask.API;
+using DCTTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCTTask.Services
+{
+    internal class CoinService
+    {
+        public async Task<IEnumerable<Coin>> GetAllCoins()
+        {
+            string url = "coins/markets?vs_currency=usd";
+            var da = new DataAccess<Coin>();
+            var coins = await da.GetData(url);
+
+            return coins;
+        }
+
+        public async Task<IEnumerable<Coin>> GetTrendingCoins()
+        {
+            string url = "search/trending";
+            var da = new DataAccess<Coin>();
+            var coins = await da.GetData(url);
+
+            return coins;
+        }
+
+        public async Task<IEnumerable<Coin>> GetCoin(string id)
+        {
+            string url = $"search/coins/{id}";
+            var da = new DataAccess<Coin>();
+            var coin = await da.GetData(url);
+
+            return coin;
+        }
+
+    }
+}
---
+++ DCTTask/Services/MarketService.cs
@@ -0,0 +1,22 @@
+﻿using DCTTask.API;
+using DCTTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCTTask.Services
+{
+    internal class MarketService
+    {
+        public async Task<IEnumerable<Market>> GetCoinMarkets(string id)
+        {
+            string url = $"coins/{id}/tickers";
+            var da = new DataAccess<Market>();
+            var markets = await da.GetData(url);
+
+            return markets;
+        }
+    }
+}
